03d755973a21a08d6065444fd704832c9af87434
Ryandesigns09 <58941197+Ryandesigns09@users.noreply.github.com>
2024-03-08 14:59:36 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -75,7 +75,7 @@ module.exports = ({ env }) => {
         filename: path.join(
           __dirname,
           '..',
-          env('DATABASE_FILENAME', 'hockey_database.db')
+      filename: env('DATABASE_FILENAME', './database/data.db'),
         ),
       },
       useNullAsDefault: true,
