40fe27d5c71fdd91e6179b20b98cad0ee76e81d0
Ryandesigns09 <58941197+Ryandesigns09@users.noreply.github.com>
2024-03-08 14:40:20 -0700

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,8 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  // Configuration for other plugins can be added here as needed
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET'),
+    },
+  },
+});
