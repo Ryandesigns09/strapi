3d9161c6fb4d6e1051cfc8d8fac846414aba6f58
Ryandesigns09 <58941197+Ryandesigns09@users.noreply.github.com>
2024-03-08 15:42:38 -0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -75,7 +75,7 @@ sqlite: {
     filename: path.join(
       __dirname,
       '..',
-      env('DATABASE_FILENAME', '/database/twodata.db'),
+      env('DATABASE_FILENAME', '/database/data.db'),
     ),
   },
   useNullAsDefault: true,
